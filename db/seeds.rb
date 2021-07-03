66ecb9b80e7172069e17c4fab809e8283aaefa23
Alexandre Bouvier <alexandre.b2506@gmail.com>
2021-07-03 10:31:28 +0200

    Lecture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant = Restaurant.new(
+  name: "My Sushi",
+  rating: 5,
+  address: "Place Sathonay"
+)
+restaurant.save
+
+
+restaurant = Restaurant.new(
+  name: "Burger King",
+  rating: 3,
+  address: "Croix Rousse"
+)
+restaurant.save
+
+restaurant = Restaurant.new(
+  name: "Brasserie L'Ouest",
+  rating: 5,
+  address: "Monts d'or"
+)
+restaurant.save
