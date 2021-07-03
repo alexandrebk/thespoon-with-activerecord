78863d8ab03c70177cd07f01374ba559f6df46a9
Alexandre Bouvier <alexandre.b2506@gmail.com>
2021-07-03 09:16:57 +0200

    Add restaurant model

---
+++ app/models/restauranrt.rb
@@ -0,0 +1,2 @@
+class Restauranrt < ApplicationRecord
+end
---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant < ApplicationRecord
+end
+
+
+
+restaurant = Restaurant.new
+Restaurant.all
+Restaurant.find(1)
+restaurant.save
+restaurant.destroy
